3ef33f7d6fef58dfb13377f6d942a02d587ee589
Apurv <apurvkhare1@gmail.com>
2020-03-28 01:03:52 +0530

    Updated for Deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ConnectDB = require("./config/db");
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
@@ -10,20 +10,18 @@ ConnectDB();
 //Init a middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to Contact Keeper API" });
-});
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 //Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
